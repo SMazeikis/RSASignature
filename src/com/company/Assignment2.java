cdd763c6e191f09b38062fb4074a912110e38e2e
SMazeikis <salvijus.mazeikis2@mail.dcu.ie>
2020-12-07 19:30:35 -0800

    Added initial variables and next steps

--- src/com/company/Assignment2.java
+++ src/com/company/Assignment2.java
@@ -1,29 +1,36 @@
 package com.company;
 
 import java.math.BigInteger;
-import java.util.Random;
+import java.math.*;
+import java.io.*;
+import javax.crypto.*;
+import java.util.*;
+import java.security.*;
+
 
 public class Assignment2 {
 
     public static void main(String[] args) {
 	// write your code here
     }
-    private String hexPrime = "b59dd79568817b4b9f6789822d22594f376e6a9abc0241846de426e5dd8f6edd" +
-    "ef00b465f38f509b2b18351064704fe75f012fa346c5e2c442d7c99eac79b2bc" +
-    "8a202c98327b96816cb8042698ed3734643c4c05164e739cb72fba24f6156b6f" +
-    "47a7300ef778c378ea301e1141a6b25d48f1924268c62ee8dd3134745cdf7323";
-    private BigInteger prime = new BigInteger(hexPrime, 16);
 
-    private BigInteger generatePrivateKey(){
-        // Generate a random object r
-        Random r = new Random();
-        // Use the constructor from BigInteger to create a randomly generated BigInteger between 0 and 2^n - 1
-        // Where n is the number of bits in prime. Use a while to loop until it is less than the original prime.
-        // i.e. compareTo returns -1 signifying it is less than prime
-        BigInteger randomInt = new BigInteger(prime.bitLength(), r);
-        while (randomInt.compareTo(prime) == 1) {
-            randomInt = new BigInteger(prime.bitLength(), r);
-        }
-        return randomInt;
-    }
+//    Generate two distinct 512-bit probable primes p and q, Miller-Rabin and Lucas-Lehmer algorithms to test primality.
+//    https://www.tutorialspoint.com/java/math/biginteger_probableprime.htm
+    BigInteger p = BigInteger.probablePrime(512, new Random());
+    BigInteger q = BigInteger.probablePrime(512, new Random());
+
+//    Calculate the product of these two primes n = pq
+
+    BigInteger n = p.multiply(q);
+
+//    Calculate the Euler totient function phi(n)
+
+//    You will be using an encryption exponent e = 65537
+    BigInteger e = new BigInteger("65537");
+//, so you will need to ensure that this is relatively prime to phi(n). If it is not, go back to step 1 and generate new values for p and q
+
+//    Compute the value for the decryption exponent d, which is the multiplicative inverse of e (mod phi(n)).
+//    This should use your own implementation of the extended Euclidean GCD algorithm to calculate the inverse rather than using a library method for this purpose.
+
+
 }
